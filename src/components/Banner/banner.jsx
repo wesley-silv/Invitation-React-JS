1c0c52942b6d9e0aa1a772e8478c396c40cca51c
Wesley Silva C <wesleysilvaconceicao@outlook.com>
2022-09-29 07:18:07 -0300

    edição das fontes, dancing script cursive.

--- src/components/Banner/banner.jsx
+++ src/components/Banner/banner.jsx
@@ -2,7 +2,8 @@ import './banner.sass';
 import {ImWhatsapp} from 'react-icons/all';
 import {ImLocation} from 'react-icons/all';
 import {GiPresent} from 'react-icons/all';
-import {IoInfiniteSharp} from 'react-icons/all';
+import {RiHeartsFill} from 'react-icons/all';
+import {FaRegHandPointDown} from 'react-icons/all';
 
 
 const Banner = () => {
@@ -12,7 +13,11 @@ const Banner = () => {
   return (
     <div className='banner'>
       <img src={perfil} alt='Fotografia de perfil dos noivos' />
-      <IoInfiniteSharp className='infinite' size={120} />
+      <section className='simbol'>
+      <hr/>
+      <RiHeartsFill className='infinite' size={120} />
+      <hr/>
+      </section>
       <h4>
         "Assim, toda a casa cobriu de ouro, até acabar toda a casa."
       </h4>
@@ -32,6 +37,7 @@ const Banner = () => {
       <p>
         Clique para acessar
       </p>
+      <FaRegHandPointDown className='FaRegHandPointDown' size={40}/>
       <div className="links_container">
         <a href='https://www.google.com.br/maps/place/Av.+Dom+Jos%C3%A9+Andr%C3%A9+Coimbra,+1520+-+Marciano+Brand%C3%A3o,+Patroc%C3%ADnio+-+MG,+38740-000/@-18.9446518,-47.0045441,15z/data=!3m1!4b1!4m5!3m4!1s0x94afba6190720015:0x1c1d6cc851af332d!8m2!3d-18.944652!4d-46.9957893' alt='Localização pel o Google Maps' title='Localização' target='blank'>
           <ImLocation size={30} />
@@ -40,7 +46,7 @@ const Banner = () => {
         <a href='https://wa.me/553492540828?text= Olá Wesley, estou entrando em contato para confirmar a minha participação em seu casamento.' alt='' title='Confirmação de presença' target='blank'><ImWhatsapp size={30}/>  Confirmar</a>
       </div>
       <a href='' alt='Logo de presentes' title='Presentear' target='blank'>
-        <GiPresent size={35}/>
+        <GiPresent size={36}/>
         Presentear
       </a>
     </div>
