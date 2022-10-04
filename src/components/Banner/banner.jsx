fff421f0ed42522d84687d30e6e9b33c31173161
Wesley Silva C <wesleysilvaconceicao@outlook.com>
2022-10-04 12:15:08 -0300

    refatoração dos links em objetos.

--- src/components/Banner/banner.jsx
+++ src/components/Banner/banner.jsx
@@ -1,4 +1,7 @@
+// Styles imports
 import './banner.sass';
+
+// Icons imports
 import {ImWhatsapp} from 'react-icons/all';
 import {ImLocation} from 'react-icons/all';
 import {GiPresent} from 'react-icons/all';
@@ -9,12 +12,21 @@ import {GiVineFlower} from 'react-icons/gi'
 
 const Banner = () => {
 
-  const perfil = '../src/assets/Meliny & Wesley.jpg'
+  const profile = {
+    imagem: '../src/assets/Meliny & Wesley.jpg',
+  }
+  const links = {
+    localization: 'https://www.google.com.br/maps/place/Av.+Dom+Jos%C3%A9+Andr%C3%A9+Coimbra,+1520+-+Marciano+Brand%C3%A3o,+Patroc%C3%ADnio+-+MG,+38740-000/@-18.9446518,-47.0045441,15z/data=!3m1!4b1!4m5!3m4!1s0x94afba6190720015:0x1c1d6cc851af332d!8m2!3d-18.944652!4d-46.9957893',
+    contact: 'https://wa.me/553492540828?text=Olá Meliny e Wesley, obrigado pelo convite, venho confirmar a minha presença no casamento de voçês.',
+    presents: '',
+    targetPage: 'blank',
+
+  }
+
   return (
     <div className='banner'>
-      <img src={perfil} alt='Fotografia de perfil dos noivos' />
+      <img src={profile.imagem} alt='Fotografia de perfil dos noivos' />
       <main className='main'>
-
       <section className='simbol'>
       <GiVineFlower size={32} color="#daa520" className='givineflower'/>
       <GiVineFlower size={32} color="#daa520" className='givineflower'/>
@@ -43,17 +55,17 @@ const Banner = () => {
         Às 19:30 horas
       </h2>
      <p>
-        Clique para acessar
+        Click para acessar
       </p>
       <FaRegHandPointDown className='FaRegHandPointDown' size={40}/>
       <div className="links_container">
-        <a href='https://www.google.com.br/maps/place/Av.+Dom+Jos%C3%A9+Andr%C3%A9+Coimbra,+1520+-+Marciano+Brand%C3%A3o,+Patroc%C3%ADnio+-+MG,+38740-000/@-18.9446518,-47.0045441,15z/data=!3m1!4b1!4m5!3m4!1s0x94afba6190720015:0x1c1d6cc851af332d!8m2!3d-18.944652!4d-46.9957893' alt='Localização pel o Google Maps' title='Localização' target='blank'>
+        <a href={links.localization} alt='Localização pel o Google Maps' title='Localização' target={links.targetPage}>
           <ImLocation size={30} />
           Localização
         </a>
-        <a href='https://wa.me/553492540828?text=Olá Meliny e Wesley, obrigado pelo convite, venho confirmar a minha presença no casamento de voçês.' alt='' title='Confirmação de presença' target='blank'><ImWhatsapp size={30}/>  Confirmar</a>
+        <a href={links.contact} alt='' title='Confirmação de presença' target={links.targetPage}><ImWhatsapp size={30}/>  Confirmar</a>
       </div>
-      <a href='' alt='Logo de presentes' title='Presentear' target='blank'>
+      <a href='' alt='Logo de presentes' title='Presentear' target={links.targetPage}>
         <GiPresent size={36}/>
         Presentear
       </a>
