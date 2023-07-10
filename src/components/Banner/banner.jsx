3d8a592c3c2dcade3d4fb8949e3152fed40b376f
Wesley Silva C <wesleysilvaconceicao@outlook.com>
2023-07-10 19:44:03 -0300

    Modificação dos icons e do conteúdo textual do subititle.

--- src/components/Banner/banner.jsx
+++ src/components/Banner/banner.jsx
@@ -3,7 +3,6 @@ import './banner.sass';
 
 // Icons imports
 import {ImWhatsapp} from 'react-icons/all';
-import {ImLocation} from 'react-icons/all';
 import {GiPresent} from 'react-icons/all';
 import {RiHeartsFill, BsFillSuitHeartFill, BsFillPinMapFill} from 'react-icons/all';
 import {FaRegHandPointDown} from 'react-icons/all';
@@ -16,10 +15,9 @@ const Banner = () => {
   }
   const links = {
     localization: 'https://www.google.com.br/maps/place/Av.+Dom+Jos%C3%A9+Andr%C3%A9+Coimbra,+1520+-+Marciano+Brand%C3%A3o,+Patroc%C3%ADnio+-+MG,+38740-000/@-18.9446518,-47.0045441,15z/data=!3m1!4b1!4m5!3m4!1s0x94afba6190720015:0x1c1d6cc851af332d!8m2!3d-18.944652!4d-46.9957893',
-    contact: 'https://wa.me/553492540828?text=Olá Meliny e Wesley, obrigado pelo convite, venho confirmar a minha presença no casamento de voçês.',
+    contact: 'https://wa.me/553492540828?text=Olá Meliny e Wesley, agradeço por este convite, será uma honra poder celebrar este momento com vocês.',
     presents: '',
     targetPage: 'blank',
-
   }
 
   return (
@@ -39,27 +37,27 @@ const Banner = () => {
       <BsFillSuitHeartFill size={32}  className='heart'/>
       </section>
       <h4 className='subtitle'>
-        "Se tardar espera-o, porque certamente virá, não tardará"
+        "Casamento"
       </h4>
       <h1 className='title'>
         Meliny Sara  <br/> & <br/> Wesley Silva
       </h1>
       <p className='text'>
-        Convidam para cerimônia de seu casamento em um culto de glorificação a Deus
+        Com a benção de Deus e de seus pais, nós, lhe convidamos a celebrar este momento de tão grande alegria conosco, em um culto de glorificação a Deus.
       </p>
       <p className='text'> A realizar-se no dia:</p>
-      <h1 className='igreja'>
+      <h1 className='date'>
         28 / Outubro / 2023
       </h1>
-      <h2>
+      <p className='hours'>
         Às 19:30 horas
-      </h2>
+      </p>
       <h1 className='title'>
         Igreja Cristã Maranata
       </h1>
-      <h2>
-        O Senhor Jesus Vem 
-      </h2>
+      <p className='maranata'>
+      Maranata! <br/> O Senhor Jesus Vem 
+      </p>
       <p className='text'>
         Toque para acessar
       </p>
@@ -67,17 +65,18 @@ const Banner = () => {
       <div className="links_container">
         <a href={links.localization} alt='Icon Point_Maps' title='Localização' target={links.targetPage}>
           <BsFillPinMapFill size={32} />
-          <br /> Localização
+          <br />
         </a>
-        <a href={links.contact} alt='Icon Whatsapp' title='Confirmação de presença' target={links.targetPage}><ImWhatsapp size={32}/>  <br /> Confirmar</a>
+        <a href={links.contact} alt='Icon Whatsapp' title='Confirmação de presença' target={links.targetPage}><ImWhatsapp size={32}/>  <br /></a>
       </div>
       <a href='' alt='Icons Present' title='Presentear' target={links.targetPage}>
         <GiPresent size={39}/>
-        <br /> Presentear
+        <br />
       </a>
-      <p className='versicleBible'>
+      <p className='end_text'>
         "Então, a nossa boca se encheu de riso, e a nossa língua, de cânticos; então, se dizia entre as nações: Grandes coisas fez o Senhor a estes. Grandes coisas fez o Senhor por nós, e, por isso, estamos alegres." <br /> Salmos 126:2
       </p>
+
       </main>
     </div>
   )
